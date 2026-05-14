5f54f60bf5ad3a8e314bda517cb8c7b18aa9521b
Maduranga128 <75471333+Maduranga128@users.noreply.github.com>
2026-05-14 12:41:23 +0530

    Delete gemini-code-1778741819485.js

--- gemini-code-1778741819485.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker: Installed');
-});
-
-self.addEventListener('fetch', (e) => {
-  // මේකෙන් ඇප් එක ඕෆ්ලයින් වුණත් වැඩ කරන්න උදව් වෙනවා
-});
